4f56cae0ce9611b67af5e5bcdcace093b7521ab3
sumitkarad07 <sumit.karad07@gmail.com>
2026-02-13 15:39:55 +0530

    Change route handling to use app.use instead of app.get

--- UMMID-Digital_Health_Wallet-main/backend/server.js
+++ UMMID-Digital_Health_Wallet-main/backend/server.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
     // Handle React routing, return all requests to React app
-    app.get('*', (req, res) => {
+    app.use('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
     });
 } else {
